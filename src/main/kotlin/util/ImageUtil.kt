3af60d1e26db2e1c474d60a48e26d0ba7b150e31
gustavo <gustavo.isaac@gmail.com>
2025-04-11 17:19:25 +0100

    Initial commit

---
+++ src/main/kotlin/util/ImageUtil.kt
@@ -0,0 +1,29 @@
+package util
+
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.toComposeImageBitmap
+import org.jetbrains.skia.Image
+import java.io.ByteArrayOutputStream
+import java.net.HttpURLConnection
+import java.net.URL
+import javax.imageio.ImageIO
+
+object ImageUtil {
+
+    fun loadNetworkImage(link: String): ImageBitmap {
+        val url = URL(link)
+        val connection = url.openConnection() as HttpURLConnection
+        connection.setRequestProperty("User-Agent", "Mozilla/5.0")
+        connection.connect()
+
+        val inputStream = connection.inputStream
+        val bufferedImage = ImageIO.read(inputStream)
+
+        val stream = ByteArrayOutputStream()
+        ImageIO.write(bufferedImage, "png", stream)
+        val byteArray = stream.toByteArray()
+
+        return Image.makeFromEncoded(byteArray).toComposeImageBitmap()
+    }
+
+}
