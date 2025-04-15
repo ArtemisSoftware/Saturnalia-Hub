8235af329e01530a44c3916c978a5090f0a46621
gustavo <gustavo.isaac@gmail.com>
2025-04-15 18:12:18 +0100

    Performance

---
+++ src/main/kotlin/domain/models/Performance.kt
@@ -0,0 +1,11 @@
+package domain.models
+
+data class Performance(
+    val date: String = "",
+    val time: String = "",
+    val name: String = "",
+    val artist: String = "",
+    val location: String = "",
+    val imageUrl: String = "",
+    val organizer: String? = null
+)
