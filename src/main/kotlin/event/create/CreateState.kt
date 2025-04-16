dca87d9b9f6cade875ab5af52ff517e4eca4dbe4
gustavo <gustavo.isaac@gmail.com>
2025-04-16 09:41:26 +0100

    CreateState

---
+++ src/main/kotlin/event/create/CreateState.kt
@@ -0,0 +1,11 @@
+package event.create
+
+import domain.models.Coordinates
+import domain.models.Performance
+import domain.models.Summary
+
+data class CreateState(
+    val summary: Summary = Summary(),
+    val performances: List<Performance> = emptyList(),
+    val coordinates: Coordinates = Coordinates()
+)
