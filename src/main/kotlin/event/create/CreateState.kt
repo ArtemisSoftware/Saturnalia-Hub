017b53d765bdf647598d8dfefc659420984a2fb6
gustavo <gustavo.isaac@gmail.com>
2025-04-21 10:07:55 +0100

    LoadFromCooltureUrlDialog

--- src/main/kotlin/event/create/CreateState.kt
+++ src/main/kotlin/event/create/CreateState.kt
@@ -5,6 +5,7 @@ import domain.models.Performance
 import domain.models.Summary
 
 data class CreateState(
+    val url: String = "",
     val summary: Summary = Summary(),
     val performances: List<Performance> = emptyList(),
     val coordinates: Coordinates = Coordinates()
---
+++ src/main/kotlin/event/create/composables/LoadFromCooltureUrlDialog.kt
@@ -0,0 +1,46 @@
+package event.create.composables
+
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+internal fun LoadFromCooltureUrlDialog(
+    url: String,
+    updateUrl: (String) -> Unit,
+    onConfirm: () -> Unit,
+    onDismiss: () -> Unit
+){
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        title = {
+            Text("Url to download")
+        },
+        text = {
+            OutlinedTextField(
+                value = url,
+                label = { Text("Coolture Url") },
+                onValueChange = updateUrl
+            )
+        },
+        confirmButton = {
+            Button(onClick = onConfirm) {
+                Text("Download")
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun LoadFromCooltureUrlDialogPreview() {
+    LoadFromCooltureUrlDialog(
+        url = "Url",
+        updateUrl = {},
+        onConfirm = {},
+        onDismiss = {},
+    )
+}
